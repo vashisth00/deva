91f1cf68863907e385e83b3fdb9cce68b8c38045
Vashisth Bhushan <vb@gounravel.com>
2021-08-04 14:36:41 +0530

    Push for edits

--- index2.js
+++ index2.js
@@ -1,4 +1,4 @@
-const arr= [1,2,3,4,5,6,7,8,9,10,2,2,1,1];
+
 
 // Find repeating elements in array
 
@@ -6,14 +6,16 @@ const arr= [1,2,3,4,5,6,7,8,9,10,2,2,1,1];
 //   const
 // }
 
-function getSum(){
-  let sum = 10;
-  for(let i = 0; i < arr.length; i++){
-    if(arr[i] + arr[i+1] == sum){
-     console.log(arr[i],arr[i+1]);
+const arr=[8,94,67,32,1,5,12]
+
+function getMax(arr){
+  let max=arr[0]
+  for(let i=1;i<arr.length;i++){
+    if(arr[i]>max){
+      max=arr[i]
     }
   }
-return arr;
+  return max
 }
 
-console.log(getSum(arr));
+console.log(getMax(arr))
